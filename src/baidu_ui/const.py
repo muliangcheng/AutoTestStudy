e6986d133b5570f04a084885b7ac68102489140c
muliangcheng <1769903432@qq.com>
2021-05-30 17:51:38 +0800

    first commit

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,3 @@
+# coding = utf-8
+
+EXCEL_DIR="/Users/sara/myclass/TestClass/TestClass/pytest-demo/data/search_query.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,6 @@
+# coding = utf-8
+BASE_URL="http://httpbin.org/"
+#BASE_URL="192.168.9.12:8080"
+IP_URL = "/ip"
+LOCAL_IP ="125.71.78.129"
+POST_TEST_URL ="/post"
